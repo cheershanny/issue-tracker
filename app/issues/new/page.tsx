c40dee5f5ea250b41cdbccd868a5416ee1d5e9ee
Hanny Tran <cheershanny@gmail.com>
2024-04-28 10:40:21 +0100

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
-import React from "react";
+import React, { useState } from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
 interface IssueForm {
   title: string;
@@ -14,30 +15,45 @@ interface IssueForm {
 const NewIssuePage = () => {
   const { register, handleSubmit, control } = useForm<IssueForm>();
   const router = useRouter();
-  return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await fetch("/api/issues", {
-          method: "POST",
-          body: JSON.stringify(data),
-        });
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root
-        placeholder="Title"
-        size="3"
-        {...register("title")}
-      ></TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => <SimpleMDE {...field} />}
-      />
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
-      <Button>Submit new Issue</Button>
-    </form>
+  const onSubmit = async (data: IssueForm) => {
+    await fetch("/api/issues", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((response) => {
+      if (response.ok) router.push("/issues");
+      else setErrorMessage("An unexpected error occurred");
+    });
+  };
+  return (
+    <div className="max-w-xl">
+      {errorMessage && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Icon>
+            <AiOutlineInfoCircle />
+          </Callout.Icon>
+          <Callout.Text>{errorMessage}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root
+          placeholder="Title"
+          size="3"
+          {...register("title")}
+        ></TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => <SimpleMDE {...field} />}
+        />
+
+        <Button>Submit new Issue</Button>
+      </form>
+    </div>
   );
 };
 
